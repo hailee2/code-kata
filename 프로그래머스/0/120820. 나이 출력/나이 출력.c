758dc33fe915d03d90be9a517b249917fdea7e65
hailee2 <slowslow9150@gmail.com>
2025-07-11 09:11:01 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int age) {
+    int answer = 0;
+    
+    if(0<age<=120){
+     
+    answer = 2022 - (age-1);    
+        
+    }   
+
+    return answer;
+}
