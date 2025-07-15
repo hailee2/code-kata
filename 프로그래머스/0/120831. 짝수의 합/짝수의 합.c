3af5f3f3ce19c1403406e7abf30ca24708243eb2
hailee2 <slowslow9150@gmail.com>
2025-07-15 09:08:53 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    for(int i = 0; i <= (n/2); i++ ){
+            answer += 2*i;}
+    return answer;
+}
