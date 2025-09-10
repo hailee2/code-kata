3c6fa584e2f92f585a0d0f218be1f15f5c1559c7
hailee2 <slowslow9150@gmail.com>
2025-09-10 10:03:05 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT CONCAT("/home/grep/src/",f.board_id,"/",f.file_id,f.file_name,f.file_ext) as file_path
+FROM used_goods_board b
+JOIN used_goods_file f
+ON b.board_id = f.board_id
+WHERE b.views = (SELECT MAX(views) FROM used_goods_board)
+ORDER BY f.file_id desc
