d2a7abe9084c35c344e92ff8eef81f18f785d599
hailee2 <slowslow9150@gmail.com>
2025-12-07 19:34:27 +0900

    Time: 1093 ms (91.94%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,24 @@
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN (
+        SELECT user_id, COUNT(*) AS cnt
+        FROM MovieRating
+        GROUP BY user_id
+    ) r ON u.user_id = r.user_id
+    ORDER BY r.cnt DESC, u.name ASC
+    LIMIT 1
+)
+UNION ALL
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN (
+        SELECT movie_id, AVG(rating) AS avg_rating
+        FROM MovieRating
+        WHERE created_at BETWEEN '2020-02-01' AND '2020-02-29'
+        GROUP BY movie_id
+    ) r ON m.movie_id = r.movie_id
+    ORDER BY r.avg_rating DESC, m.title ASC
+    LIMIT 1
+);
